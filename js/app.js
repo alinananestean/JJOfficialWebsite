9ec716c566d80843f34fcd2fce1023e6247ce318
DaniNanestean <dani.nanesteandani.nanesteangit config user.email dani.nanestean@yahoo.comgit config user.email dani.nanesteandani.nanestean>
2017-03-31 00:16:32 +0300

    Slightly adjusted the padding on desktop Added new accent color, contact hero img, form, news sections

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ $(document).foundation();
 
 $( document ).ready(function() {
 
+    //Scroll Arrows
+
     $('.scroll-arrows').find('a').click(function(e){
         e.preventDefault();
         var offset = -110;
@@ -10,6 +12,16 @@ $( document ).ready(function() {
         $('body,html').animate({
             scrollTop: anchor.top + offset
         }, 800);
+    });
+
+
+    //On input focus label changes color
+
+    $("form :input").focus(function() {
+        $("label[for='" + this.id + "']").addClass("labelfocus");
+
+    }).blur(function() {
+        $("label").removeClass("labelfocus");
     });
 
 });
